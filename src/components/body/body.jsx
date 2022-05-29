e87f515fbdd48625abbf962cf23d10131deb64d3
Lily-956 <99384077+Lily-956@users.noreply.github.com>
2022-05-29 13:04:51 +0900

    more touches

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,8 +1,33 @@
+import { Typography } from '@mui/material'
+import { Card, CardContent } from "@mui/material"
+import { Container } from "@mui/system"
+
 function Body() {
     return (
-        <div>
-            This is a body component
-        </div>
+       <Container className="body">
+           <Card sx={{maxWidth: "500px"}}>
+               <CardContent>
+                   <Typography variant="h6">
+                       Cheese Cake
+                   </Typography>
+                   <Typography>
+                       Cheese cake is an amazing dessert! 
+                   </Typography>
+               </CardContent>
+           </Card>
+        
+           <Card sx={{maxWidth: "500px"}}>
+               <CardContent>
+                   <Typography variant="h6">
+                       Cheese Cake
+                   </Typography>
+                   <Typography>
+                       Cheese cake is an amazing dessert! 
+                   </Typography>
+               </CardContent>
+           </Card>
+
+       </Container>
     )
 }
 
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,12 @@
+import { Typography } from '@mui/material'
+import './footer.css'
+
 function Footer() {
     return (
-        <div>
-            Copy rights by Lily. All rights reserved
+        <div className="footer">
+            <Typography>
+                &copy; {new Date().getFullYear()} Lily Watanabe. All Rights Reserved.
+            </Typography>
         </div>
     )
 }
