c16842b7f618877c5d0f214a76f6126e87147115
Lily-956 <99384077+Lily-956@users.noreply.github.com>
2022-06-12 10:18:35 +0900

    more edits for about and paragraph

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -6,13 +6,15 @@ function About() {
             <h3>
                 Welcome! This page will be an overview about Despicable Me!
             </h3>
-            <img className="about" src="https://s3.amazonaws.com/static.rogerebert.com/uploads/movie/movie_poster/despicable-me-2010/large_4zHJhBSY4kNZXfhTlmy2TzXD51M.jpg" alt="Minion ovewview poster" />
-            <p>
+            <img className="about" src="https://alternativemovieposters.com/wp-content/uploads/2013/10/despicablebg.jpg" alt="Minion ovewview poster" />
+            <span >
             Supervillain Gru, a man who delights in all things wicked, hatches a plan to steal the moon. 
-            Surrounded by an army of little yellow minions and his impenetrable arsenal of weapons and war machines, Gru prepares to vanquish all who stand in his way. 
-            However, nothing in his calculations and groundwork has prepared him for his greatest challenge: three adorable orphan girls who want to make him their dad. -Google
+            </span>
+            <p className="paragraph">
+            Surrounded by an army of little yellow minions and his impenetrable arsenal of weapons and war machines, Gru prepares to vanquish all who stand in his way. However, nothing in his calculations and groundwork has prepared him for his greatest challenge: three adorable orphan girls who want to make him their dad. -Google
             </p>
         </div>
+
     )
 }
 
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,7 +2,7 @@ import './home.css'
 
 function Home() {
     return (
-        <div>
+        <div className="life">
             <h2>
             Welcome to my website! I hope you enjoy your stay  ~(˘▾˘~)
             </h2>
@@ -11,7 +11,7 @@ function Home() {
             </h3>
             <img className="home" src="https://cdn.shopify.com/s/files/1/0747/3829/products/mHP1567_1024x1024.jpeg?v=1571444587" alt="Minion poster" />
             <h4>
-            This website will contain an overview, character descrptions, movie summaries, and fun facts about the movie
+            This website will contain an overview, character descrptions, movie summaries of Despicable Me!
             </h4>
         </div>
     )
